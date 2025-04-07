50779196be8b3a71fd3c44b1d847eea6e9938acf
carolmeireles <carolinemeireles.si@gmail.com>
2025-04-07 16:51:59 -0300

    estilização da seção sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,11 @@ const Sobre = () => (
       Nisi perferendis laudantium accusamus repellat quam enim placeat voluptate
       rem.
     </Paragrafo>
+
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=carolmeireles&show_icons=true&theme=dracula&include_all_commits=true&count_private=false" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=carolmeireles&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
