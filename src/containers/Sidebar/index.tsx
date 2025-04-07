555408004db28eb3a4b69ab05459665f59cd5352
carolmeireles <carolinemeireles.si@gmail.com>
2025-04-07 16:09:02 -0300

    criação de componentes de texto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/carolmeireles.png" />
+    <Titulo fontSize={20}>Caroline Meireles</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit recusandae
+      quidem illo eveniet repellat architecto soluta corporis dicta sequi nulla.
+      Nisi perferendis laudantium accusamus repellat quam enim placeat voluptate
+      rem.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
