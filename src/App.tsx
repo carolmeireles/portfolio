b5f1af9c4f3fc7f2d5cc3d8cec475c78b649da39
carolmeireles <carolinemeireles.si@gmail.com>
2025-04-08 17:37:47 -0300

    erro no tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components'
 import EstiloGlobal, { Container } from './styles'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import Projetos from './containers/Projetos'
+import temaLight from './themes/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaLight}>
       <EstiloGlobal />
       <Container>
         <Sidebar />
@@ -14,7 +16,7 @@ function App() {
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
